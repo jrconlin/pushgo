377562889c6f6f4ee8f819fb130a9beb80a0c4ef
jrconlin <jconlin+git@mozilla.com>
2013-05-29 09:33:24 -0700

    Post AWS install tweaks

--- install.bash
+++ install.bash
@@ -12,7 +12,7 @@ export GOPATH=`pwd`
 echo "Installing required go libraries..."
 for req in `cat go_deps.lst`; do
     echo -n "   $req..."
-    go get -v $req
+    go get -v
     echo " done"
 done
 echo "Libraries installed"
