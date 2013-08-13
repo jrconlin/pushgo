c09fd26d3c4f994537230849c7f85a6d73d03d63
jrconlin <jconlin+git@mozilla.com>
2013-08-13 10:43:08 -0700

    Move iterator holder outside loop to appease gc

--- src/mozilla.org/simplepush/storage/mcstorage/mcstorage.go
+++ src/mozilla.org/simplepush/storage/mcstorage/mcstorage.go
@@ -437,8 +437,8 @@ func (self *Storage) GetUpdates(uaid string, lastAccessed int64) (results util.J
 
 	// Result has no len or counter.
 	resCount := 0
+	var i string
 	for _, key := range items {
-		var i string
 		if err := recs.Get(key, &i); err == nil {
 			resCount = resCount + 1
 		}
