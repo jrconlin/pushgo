0ff550f2752f3c31b7e456bbd00bf10c61449424
jrconlin <jconlin+git@mozilla.com>
2014-10-16 09:57:27 -0700

    Added per @ulfr request.
    No longer accept SSLv3 (since it's been critically compromised)

--- src/github.com/mozilla-services/pushgo/simplepush/net.go
+++ src/github.com/mozilla-services/pushgo/simplepush/net.go
@@ -116,6 +116,19 @@ func ListenTLS(addr, certFile, keyFile string, maxConns int, keepAlivePeriod tim
 	config := &tls.Config{
 		NextProtos:   []string{"http/1.1"},
 		Certificates: []tls.Certificate{cert},
+		// The following are Mozilla required TLS settings.
+		MinVersion: tls.VersionTLS10,
+		CipherSuites: []uint16{
+			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
+			tls.TLS_RSA_WITH_AES_128_CBC_SHA,
+			tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+			tls.TLS_ECDHE_RSA_WITH_3DES_EDE_CBC_SHA,
+			tls.TLS_RSA_WITH_3DES_EDE_CBC_SHA},
 	}
 	return tls.NewListener(&LimitListener{ln.(*net.TCPListener), maxConns,
 		0, keepAlivePeriod}, config), nil
