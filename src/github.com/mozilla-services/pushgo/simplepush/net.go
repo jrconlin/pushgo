4d9c9407c673fdefef254b101eb6996d58b209bc
jrconlin <jconlin+git@mozilla.com>
2014-10-16 10:14:16 -0700

    BUG: #95 Added server cipher preference & formatting

--- src/github.com/mozilla-services/pushgo/simplepush/net.go
+++ src/github.com/mozilla-services/pushgo/simplepush/net.go
@@ -117,7 +117,8 @@ func ListenTLS(addr, certFile, keyFile string, maxConns int, keepAlivePeriod tim
 		NextProtos:   []string{"http/1.1"},
 		Certificates: []tls.Certificate{cert},
 		// The following are Mozilla required TLS settings.
-		MinVersion: tls.VersionTLS10,
+		MinVersion:               tls.VersionTLS10,
+		PreferServerCipherSuites: true,
 		CipherSuites: []uint16{
 			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
 			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
