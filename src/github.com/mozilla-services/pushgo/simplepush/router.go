99f53070aa06ab0716333c3d01e30acae3737612
Kit Cambridge <github@kitcambridge.be>
2014-11-11 09:20:51 -0800

    Revert to `io.Pipe()`.

--- src/github.com/mozilla-services/pushgo/simplepush/router.go
+++ src/github.com/mozilla-services/pushgo/simplepush/router.go
@@ -17,7 +17,6 @@
 package simplepush
 
 import (
-	"bytes"
 	"crypto/tls"
 	"errors"
 	"fmt"
@@ -323,15 +322,9 @@ func (r *Router) notifyBucket(cancelSignal <-chan bool, contacts []string,
 func (r *Router) notifyContact(result chan<- bool, stop <-chan struct{},
 	url string, segment *capn.Segment, logID string) {
 
-	body := new(bytes.Buffer)
-	if _, err := segment.WriteTo(body); err != nil {
-		if r.logger.ShouldLog(ERROR) {
-			r.logger.Error("router", "Error buffering router request",
-				LogFields{"rid": logID, "error": err.Error()})
-		}
-		return
-	}
-	req, err := http.NewRequest("PUT", url, body)
+	reader, writer := io.Pipe()
+	go pipeTo(writer, segment)
+	req, err := http.NewRequest("PUT", url, reader)
 	if err != nil {
 		if r.logger.ShouldLog(ERROR) {
 			r.logger.Error("router", "Router request failed",
@@ -381,4 +374,11 @@ func (r *Router) runLoop() {
 			run()
 		}
 	}
+}
+
+func pipeTo(dest io.WriteCloser, src io.WriterTo) (err error) {
+	if _, err = src.WriteTo(dest); err != nil {
+		return
+	}
+	return dest.Close()
 }
