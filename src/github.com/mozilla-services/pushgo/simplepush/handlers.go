589677e757c3c59b5125bc70071716ae2b14cea3
Ben Bangert <ben@groovie.org>
2014-11-14 15:59:59 -0800

    Don't set the header till later due to setting a status header if a router fails out badly.

--- src/github.com/mozilla-services/pushgo/simplepush/handlers.go
+++ src/github.com/mozilla-services/pushgo/simplepush/handlers.go
@@ -297,7 +297,6 @@ sendUpdate:
 		http.Error(resp, "Could not update channel version", status)
 		return
 	}
-	resp.Header().Set("Content-Type", "application/json")
 	self.metrics.Increment("updates.appserver.received")
 
 	// Ping the appropriate server
@@ -320,6 +319,7 @@ sendUpdate:
 	if clientConnected {
 		self.app.Server().RequestFlush(client, chid, int64(version))
 	}
+	resp.Header().Set("Content-Type", "application/json")
 	resp.Write([]byte("{}"))
 	return
 }
