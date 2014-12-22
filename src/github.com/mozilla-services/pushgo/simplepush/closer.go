374904d076c3413733028fca89d04418639feee3
Kit Cambridge <github@kitcambridge.be>
2014-12-22 16:41:32 -0700

    Add docs for `Closer`.

--- src/github.com/mozilla-services/pushgo/simplepush/closer.go
+++ src/github.com/mozilla-services/pushgo/simplepush/closer.go
@@ -8,19 +8,25 @@ import (
 	"sync/atomic"
 )
 
+// CloserOnce describes a closable resource.
 type CloserOnce interface {
 	CloseOnce() error
 }
 
+// Closable is an io.Closer that ensures the underlying resource is closed
+// exactly once. It's similar to sync.Once, and safe for concurrent use.
 type Closable struct {
 	CloserOnce
 	closed int32 // Accessed atomically.
 }
 
+// IsClosed indicates whether the resource is closed. sync.Once does not
+// expose this information.
 func (c *Closable) IsClosed() bool {
 	return atomic.LoadInt32(&c.closed) == 1
 }
 
+// Close closes the resource, returning nil if invoked multiple times.
 func (c *Closable) Close() error {
 	if atomic.CompareAndSwapInt32(&c.closed, 0, 1) {
 		return c.CloseOnce()
